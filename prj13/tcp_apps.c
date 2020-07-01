a4fd9dd4b1542c483bd2750454dccb485e6b52c4
Yu <cacaogattoxy@gmail.com>
2020-07-01 21:48:16 +0800

    prj13 finished(coredump sometimes)

---
+++ prj13/tcp_apps.c
@@ -0,0 +1,182 @@
+#include "tcp_sock.h"
+
+#include "log.h"
+
+#include <stdlib.h>
+#include <unistd.h>
+
+#define CONTENT_SIZE 1000
+
+#define FILE_TRANSFER
+#define NO_ECHO
+
+#ifdef FILE_TRANSFER
+#define INPUT_NAME "client-input.dat"
+#define OUTPUT_NAME "server-output.dat"
+#endif
+
+
+// tcp server application, listens to port (specified by arg) and serves only one
+// connection request
+void *tcp_server(void *arg)
+{
+	u16 port = *(u16 *)arg;
+	struct tcp_sock *tsk = alloc_tcp_sock();
+
+	struct sock_addr addr;
+	addr.ip = htonl(0);
+	addr.port = port;
+	if (tcp_sock_bind(tsk, &addr) < 0) {
+		log(ERROR, "tcp_sock bind to port %hu failed", ntohs(port));
+		exit(1);
+	}
+
+	if (tcp_sock_listen(tsk, 3) < 0) {
+		log(ERROR, "tcp_sock listen failed");
+		exit(1);
+	}
+
+	log(DEBUG, "listen to port %hu.", ntohs(port));
+
+	struct tcp_sock *csk = tcp_sock_accept(tsk);
+
+	log(DEBUG, "accept a connection.");
+
+	char rbuf[CONTENT_SIZE + 1];
+    int rlen = 0;
+
+#ifdef FILE_TRANSFER
+    printf("Begin to receive %s from client.\n", OUTPUT_NAME);
+    FILE *fp = fopen(OUTPUT_NAME, "w+");
+    unsigned long wlen = 0;
+    while (1) {
+        memset(rbuf, 0, sizeof(rbuf));
+        rlen = tcp_sock_read(csk, rbuf, CONTENT_SIZE);
+        if (rlen == 0) {
+            log(DEBUG, "tcp_sock_read return 0, finish transmission.");
+            break;
+        }
+        else if (rlen > 0) {
+#ifndef NO_ECHO
+            rbuf[rlen] = '\0';
+            printf("[Recv] %s\n", rbuf);
+#endif
+            wlen = fwrite(rbuf, sizeof(char), rlen, fp);
+            fflush(fp);
+            if (wlen < rlen) {
+                printf("File writing fail!\n");
+                break;
+            }
+        }
+        else {
+            log(DEBUG, "tcp_sock_read return negative value, something goes wrong.");
+            exit(1);
+        }
+    }
+    // printf("File received! Final rlen: %d ; Final wlen: %lu\n", rlen, wlen);
+    sleep(1);
+    fclose(fp);
+#else
+	char wbuf[1024];
+
+	while (1) {
+		rlen = tcp_sock_read(csk, rbuf, CONTENT_SIZE);
+		if (rlen == 0) {
+			log(DEBUG, "tcp_sock_read return 0, finish transmission.");
+			break;
+		} 
+		else if (rlen > 0) {
+			rbuf[rlen] = '\0';
+			sprintf(wbuf, "server echoes: %s", rbuf);
+			if (tcp_sock_write(csk, wbuf, strlen(wbuf)) < 0) {
+				log(DEBUG, "tcp_sock_write return negative value, something goes wrong.");
+				exit(1);
+			}
+		}
+		else {
+			log(DEBUG, "tcp_sock_read return negative value, something goes wrong.");
+			exit(1);
+		}
+	}
+#endif
+
+	log(DEBUG, "close this connection.");
+
+	tcp_sock_close(csk);
+	
+	return NULL;
+}
+
+// tcp client application, connects to server (ip:port specified by arg), each
+// time sends one bulk of data and receives one bulk of data 
+void *tcp_client(void *arg)
+{
+	struct sock_addr *skaddr = arg;
+
+	struct tcp_sock *tsk = alloc_tcp_sock();
+
+	if (tcp_sock_connect(tsk, skaddr) < 0) {
+		log(ERROR, "tcp_sock connect to server ("IP_FMT":%hu)failed.", \
+				NET_IP_FMT_STR(skaddr->ip), ntohs(skaddr->port));
+		exit(1);
+	}
+
+    char rbuf[CONTENT_SIZE + 1];
+    int rlen = 0;
+
+#ifdef FILE_TRANSFER
+    FILE *fp = fopen(INPUT_NAME, "r");
+    if (fp) {
+        printf("Begin to transfer %s to server.\n", INPUT_NAME);
+        char wbuf[CONTENT_SIZE];
+        int wlen;
+        memset(wbuf, 0, sizeof(wbuf));
+        while ((wlen = fread(wbuf, sizeof(char), CONTENT_SIZE, fp)) > 0) {
+#ifndef NO_ECHO
+            printf("[Send] %s\n", wbuf);
+#endif
+            if (tcp_sock_write(tsk, wbuf, wlen) < 0) {
+                printf("Unexpected ERROR!!!!!!\n");
+                break;
+            }
+            // usleep(1000);
+            memset(wbuf, 0, sizeof(wbuf));
+            if (feof(fp)) break;
+        }
+        fclose(fp);
+        printf("Transfer finished.\n");
+    }
+    else {
+        printf("File %s not found.\n", INPUT_NAME);
+    }
+    sleep(1);
+#else
+    char *wbuf = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int wlen = strlen(wbuf);
+
+	int n = 10;
+	for (int i = 0; i < n; i++) {
+		if (tcp_sock_write(tsk, wbuf + i, wlen - n) < 0)
+			break;
+
+		rlen = tcp_sock_read(tsk, rbuf, CONTENT_SIZE);
+		if (rlen == 0) {
+			log(DEBUG, "tcp_sock_read return 0, finish transmission.");
+			break;
+		}
+		else if (rlen > 0) {
+			rbuf[rlen] = '\0';
+			fprintf(stdout, "%s\n", rbuf);
+		}
+		else {
+			log(DEBUG, "tcp_sock_read return negative value, something goes wrong.");
+			exit(1);
+		}
+		sleep(1);
+	}
+#endif
+
+	tcp_sock_close(tsk);
+
+	return NULL;
+}
---
+++ prj13/tcp_timer.c
@@ -0,0 +1,97 @@
+#include "tcp.h"
+#include "tcp_timer.h"
+#include "tcp_sock.h"
+
+#include <stdio.h>
+#include <unistd.h>
+
+static struct list_head timer_list;
+pthread_mutex_t timer_lock;
+
+// scan the timer_list, find the tcp sock which stays for at 2*MSL, release it
+void tcp_scan_timer_list()
+{
+	// TODO: implement %s please.\n, __FUNCTION__
+	struct tcp_timer *pos, *q;
+    pthread_mutex_lock(&timer_lock);
+	list_for_each_entry_safe(pos, q, &timer_list, list) {
+	    if (pos->type == 0) {
+            pos->timeout -= TCP_TIMER_SCAN_INTERVAL;
+            if (pos->timeout <= 0) {
+                list_delete_entry(&pos->list);
+                struct tcp_sock *tsk = timewait_to_tcp_sock(pos);
+                tcp_set_state(tsk, TCP_CLOSED);
+                if (!tsk->parent) tcp_bind_unhash(tsk);
+                tcp_unhash(tsk);
+            }
+        } else {
+	        struct tcp_sock *tsk = retranstimer_to_tcp_sock(pos);
+	        struct send_buffer *buf;
+	        list_for_each_entry(buf, &tsk->send_buf, list) {
+                buf->timeout -= TCP_TIMER_SCAN_INTERVAL;
+	            if (!buf->timeout) {
+	                if (buf->times++ == 3) {
+	                    printf("Packet Loss!!!\n");
+	                    buf->times = 1;
+	                    buf->timeout = TCP_RETRANS_INTERVAL_INITIAL;
+	                } else {
+	                    char *temp = (char*)malloc(buf->len * sizeof(char));
+	                    memcpy(temp, buf->packet, buf->len);
+	                    ip_send_packet(temp, buf->len);
+	                    if (buf->times == 2) buf->timeout = 2 * TCP_RETRANS_INTERVAL_INITIAL;
+	                    else buf->timeout = 4 * TCP_RETRANS_INTERVAL_INITIAL;
+	                }
+	            }
+	        }
+	    }
+	}
+    pthread_mutex_unlock(&timer_lock);
+}
+
+// set the timewait timer of a tcp sock, by adding the timer into timer_list
+void tcp_set_timewait_timer(struct tcp_sock *tsk)
+{
+	// TODO: implement %s please.\n, __FUNCTION__
+	tsk->timewait.type = 0;
+	tsk->timewait.timeout = TCP_TIMEWAIT_TIMEOUT;
+    pthread_mutex_lock(&timer_lock);
+	list_add_tail(&tsk->timewait.list, &timer_list);
+    pthread_mutex_unlock(&timer_lock);
+	tsk->ref_cnt++;
+}
+
+void tcp_set_retrans_timer(struct tcp_sock *tsk) {
+    tsk->retrans_timer.type = 1;
+    tsk->retrans_timer.timeout = TCP_RETRANS_INTERVAL_INITIAL;
+    pthread_mutex_lock(&timer_lock);
+    struct tcp_timer *pos;
+    list_for_each_entry(pos, &timer_list, list) {
+        if (retranstimer_to_tcp_sock(pos) == tsk) {
+            pthread_mutex_unlock(&timer_lock);
+            return;
+        }
+    }
+    list_add_tail(&tsk->retrans_timer.list, &timer_list);
+    pthread_mutex_unlock(&timer_lock);
+    tsk->ref_cnt++;
+}
+
+void tcp_unset_retrans_timer(struct tcp_sock *tsk) {
+    pthread_mutex_lock(&timer_lock);
+    list_delete_entry(&tsk->retrans_timer.list);
+    pthread_mutex_unlock(&timer_lock);
+    tsk->ref_cnt--;
+}
+
+// scan the timer_list periodically by calling tcp_scan_timer_list
+void *tcp_timer_thread(void *arg)
+{
+	init_list_head(&timer_list);
+	pthread_mutex_init(&timer_lock, NULL);
+	while (1) {
+		usleep(TCP_TIMER_SCAN_INTERVAL);
+		tcp_scan_timer_list();
+	}
+
+	return NULL;
+}
